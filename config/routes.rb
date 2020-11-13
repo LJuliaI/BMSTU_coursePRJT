c59cc84ddf8a7d599d425f51b18e04712ad678ba
Юлия Лях <yanawinx@list.ru>
2020-11-13 19:21:53 +0300

    working master with DB

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get 'map/bmstuMap'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'map' => 'map#bmstuMap'
   resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

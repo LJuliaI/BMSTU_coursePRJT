99aad1c7b6426dc4e6f066ef066e93925879a55f
Юлия Лях <yanawinx@list.ru>
2020-11-20 00:35:02 +0300

    gallery of all floors

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,12 @@
 class MapController < ApplicationController
+
   def bmstuMap
   #  room = Room.new
+@viewbox = "0 0 1382.6667 974.66669"
     @rooms = Room.selectroomsbyfloor (2)
+  end
+  def  floorBtn(flor)
+
+      @rooms = Room.selectroomsbyfloor (flor)
   end
 end

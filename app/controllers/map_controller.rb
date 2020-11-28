8f6761c223e57eab4ab139fb575dda07d0bd03cc
Ksobr <geroy2000@mail.ru>
2020-11-28 23:27:40 +0300

    Kostil

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -3,7 +3,8 @@ class MapController < ApplicationController
   def bmstuMap
   # room = Room.new
 #  @viewbox = "0 0 1382.6667 974.66669"
-    @rooms = Room.selectroomsbyfloor (2)
+    @rooms2 = Room.selectroomsbyfloor (2)
+    @rooms3 = Room.selectroomsbyfloor (3)
   end
 #  def  floorBtn(flor)
 

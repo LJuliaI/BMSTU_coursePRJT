cb110d0db415ca8f98d70cb16ab4659fdf9b07eb
Ksobr <geroy2000@mail.ru>
2020-11-29 03:35:22 +0300

    raw

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,13 +1,15 @@
 class MapController < ApplicationController
 
   def bmstuMap
-  # room = Room.new
-#  @viewbox = "0 0 1382.6667 974.66669"
-    @rooms2 = Room.selectroomsbyfloor (2)
-    @rooms3 = Room.selectroomsbyfloor (3)
+    @rooms2 =  Room.selectroomsbyfloor (2)
   end
-#  def  floorBtn(flor)
-
-#      @rooms = Room.selectroomsbyfloor (flor)
-#  end
+  def  getdata()
+    @rooms =  Room.selectroomsbyfloor(params["etaz"].to_i).as_json
+      respond_to do |format|
+        format.html
+        format.json do render json:
+          @rooms
+        end
+      end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'map' => 'map#bmstuMap'
+  get 'map/getdata'
   resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
